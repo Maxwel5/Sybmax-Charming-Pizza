464340629e4dc10df22241dae07f0a62f90046de
Maxwel5 <maxwellwafulasim@gmail.com>
2019-07-31 09:26:02 +0300

    Analizing errors

--- README.md
+++ README.md
@@ -9,21 +9,21 @@ Currently a student at Moringa school parsuing *Software Development*
 
 ## Installation
 
-    /* open terminal(Ctrl+Alt+T)
-    /* link: https://github.com/Maxwel5/Sybmax-Charming-Pizza.git   
-    /* pizza website
-    /* code: It is constituted of the terminologies mentioned above which have potentially collaborated to give direction through forms to the client/customer on the user interface. It is also composed of bussiness logic to for effective perfomance.
+    * open terminal(Ctrl+Alt+T)
+    * link: https://github.com/Maxwel5/Sybmax-Charming-Pizza.git   
+    * pizza website
+    * code: It is constituted of the terminologies mentioned above which have potentially collaborated to give direction through forms to the client/customer on the user interface. It is also composed of bussiness logic to for effective perfomance.
 
 ## Terminologies used:
-/* *HTML*
-/* *CSS*
-/* *JS*
-/* *jQUERY*
+* *HTML*
+* *CSS*
+* *JS*
+* *jQUERY*
 
 ## Behaviour Driven Development (BDD)
 Given the form on the user interface, the user is able to order the the size, crust and topping of the pizza. When he/she has ordered what they want it perfoms the calcultion after which then it output the the total ammount to be paid.
 
 ## Lisence
-/* MIT Lisence.
-/* Permision is granted to the users and necessarary corrections be addressed with any other test to make it more appealing. 
+* MIT Lisence.
+* Permision is granted to the users and necessarary corrections be addressed with any other test to make it more appealing. 
 copyright&copy;2019maxwelwafula
--- js/scripts.js
+++ js/scripts.js
@@ -3,48 +3,80 @@ var pizzaSize;
 var pizzaCrusts;
 var pizzaToppings;
 var sizePrices;
-var totalTops=0;
+var totalToppingCost = 0;
+var pizzaCost = 0;
 
 
-function PizzaConstruct(size, crust, topping) {
+
+
+
+function PizzaConstruct(size, crust, topping, total) {
   this.pizzaSize = size;
   this.pizzaCrust = crust;
   this.pizzaTopping = topping;
+  this.total = total;
 }
-PizzaConstruct.prototype.calcPrice = function() {
-  return this.pizzaSize + this.pizzaCrust + pizzaTopping;
+PizzaConstruct.prototype.calcPrice = function () {
+  return this.pizzaSize + this.pizzaCrusts + pizzaToppings;
 };
 
 
-$(document).ready(function(){
-  $("form#pizza").submit(function(event){
+$(document).ready(function () {
+  $("#checkout").hide();
+  $("#show-contact").hide();
+
+  $("#add").click(function (event) {
+    $("#checkout").show();
+    $("#add").hide();
+
+
     event.preventDefault()
 
-    var sizePrices=parseInt($("select#siz").children("option:selected").val());
-    
-    // function test(){
-    //   alert(sizePrices);
-    // }
-    // var pizzaPrices= $("#siz").children("option").filter(":checked").val();
-    var crustPrice=$("input[name='crust']:checked").val();
-    var totalTops=[];
-    var totalToppingCost=0;
-    $("input[name='top']:checked").each(function(){
-     totalTops.push(parseInt($(this).val()));
+    var sizePrices = parseInt($("select#siz").children("option:selected").val());
+    var crustPrice = parseInt($("select#crust").children("option:selected").val());
+    var quantity = $("#myInput").val();
 
-     var totalToppingCost=totalTops.reduce((totalToppingCost,totalTops)=>totalToppingCost+totalTops);
-     
-      });
-    function test(){
-      alert(sizePrices);
+    console.log(sizePrices);
+    console.log(crustPrice);
+
+    var totalTops = [];
+
+    $.each($("input[name='top']:checked"), function () {
+      totalTops.push($(this).val());
+    });
+
+
+    function totalPizzaPrice() {
+      var pizzaCost = sizePrices + crustPrice + (totalTops.length * 100);
+      pizzaCost = pizzaCost * quantity;
+      console.log(pizzaCost);
     }
-    test();
-   
+    totalPizzaPrice();
+
+    $("#checkout").click(function (event) {
+      event.preventDefault();
+      $("#show-contact").show();
+
+      $("#p-size").append(sizePrices);
+      $("#p-crust").append(crustPrice);
+      $("#p-topping").append((totalTops.length * 100));  
+      $("#t-price").append(pizzaCost);
+    });
     
   });
   $("#btn1").click(function(){
-    var name = $("#name").val()   
-    $(".output").text(name);
+    event.preventDefault();
+    var name_data = document.getElementById("#name").value
+    $("#output1").text(name_data);
+    var location_data = document.getElementById("#location").value
+    $("#output1").text(location_data)
+    var email_data = document.getElementById("#email").value
+    $("#output1").text(email_data)
+    var phone_data = document.getElementById("#phone").value
+    $("#output1").text(phone_data)
+    
 
+    
   })
+
 });
